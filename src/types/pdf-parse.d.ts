b777a2c1b5c097fe152cb7d994cf72e8057034bd
Jiaguan <tianlanp@gmail.com>
2026-05-25 05:09:16 +1000

    chore: pdf-parse类型声明

---
+++ src/types/pdf-parse.d.ts
@@ -0,0 +1,12 @@
+declare module 'pdf-parse' {
+  interface PDFParseResult {
+    numpages: number
+    numrender: number
+    info: Record<string, unknown>
+    metadata: Record<string, unknown>
+    text: string
+    version: string
+  }
+  function pdfParse(buffer: Buffer): Promise<PDFParseResult>
+  export default pdfParse
+}
